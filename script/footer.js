0c94ec0b5a4ce5c589b13dfa82ba68ea7f47626a
Eduardo P <edtvports@gmail.com>
2022-10-21 00:31:13 -0300

    Footer

---
+++ script/footer.js
@@ -0,0 +1,18 @@
+document.write(`
+<footer class="footer">
+    <div class="footerText">
+        <p>Este site foi feito como avaliação da disciplina de DPW (Desenvolvimento de Páginas Web) para o Bacharelado de Sistemas de Informação da UNIRIO.</p>
+        <p>Feito por Eduardo P. 2022</p>
+        <div class="footerIcon">
+            <img src="./assets/hockey.svg" class="icon">
+            <h2 class="footerText"> Hockey</h2>
+        </div>
+        <div class="footerIcon">
+            <a href="http://www.unirio.br/" target="unirio" class="footerText">Unirio</a>
+            <a href="https://bsi.uniriotec.br/" target="unirio" class="footerText">BSI-Unirio</a>
+        </div>
+    </div>
+</footer>
+`);
+
+document.getElementById("footerScript").remove();
---
+++ script/navbar.js
@@ -0,0 +1,15 @@
+document.write(`
+<header>
+    <nav class="navbar">
+        <img src="./assets/hockey.svg" class="icon">
+        <h2 class="navHeader">Hockey</h2>
+        <a href="index.html" class="navText">Início</a>
+        <a href="historia.html" class="navText">História</a>
+        <a href="curiosidades.html" class="navText">Curiosidades</a>
+        <a href="quiz.html" class="navText">Quiz</a>
+    </nav>
+</header>
+
+`);
+
+document.getElementById("navScript").remove();
